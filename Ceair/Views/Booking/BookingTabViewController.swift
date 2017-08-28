137f349c5942d0417ffa01ec05c4745343b60956
matt <matt@bizersoft.com>
2017-08-29 07:56:32 +0800

    Init Ceair

---
+++ Ceair/Views/Booking/BookingTabViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BookingTabViewController.swift
+//  Ceair
+//
+//  Created by Tong Tian on 8/29/17.
+//  Copyright © 2017 Bizersoft. All rights reserved.
+//
+
+import UIKit
+
+class BookingTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+}
---
+++ Ceair/Views/TabBarViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TabBarViewController.swift
+//  Ceair
+//
+//  Created by Tong Tian on 8/29/17.
+//  Copyright © 2017 Bizersoft. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let bookingTab = UINavigationController(rootViewController: BookingTabViewController())
+        
+        let dummyTabOne = createDummyTabItem(title: "One")
+        let dummyTabTwo = createDummyTabItem(title: "Two")
+        let dummyTabThree = createDummyTabItem(title: "Three")
+        
+        viewControllers = [bookingTab, dummyTabOne, dummyTabTwo, dummyTabThree]
+    }
+    
+    private func createDummyTabItem(title: String) -> UINavigationController {
+        let dummyViewController = UIViewController()
+        dummyViewController.view.backgroundColor = UIColor.fromHEX(string: "#ABCDEF")
+        
+        let navController = UINavigationController(rootViewController: dummyViewController)
+        navController.tabBarItem.title = title
+        navController.navigationItem.title = title
+        
+        return navController
+    }
+
+}
