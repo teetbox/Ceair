d9c5b8749f2cfae73f66d670e253de8a2272ed02
matt <matt@bizersoft.com>
2017-08-30 12:51:36 +0800

    Set status bar style to lightContent for flight tab

--- Ceair/Views/Flight/FlightTabViewController.swift
+++ Ceair/Views/Flight/FlightTabViewController.swift
@@ -18,6 +18,18 @@ class FlightTabViewController: UIViewController {
         setupUI()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        UIApplication.shared.statusBarStyle = .lightContent
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        UIApplication.shared.statusBarStyle = .default
+    }
+    
     private func setupUI() {
         let backgroundImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
         let fligtTabBackgroundImage = UIImage(named: Images.FlightBackground)
